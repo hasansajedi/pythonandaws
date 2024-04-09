895d46d252fd1173da7d7bff4c5aba798ac0db26
Hasan <hassansajedi@gmail.com>
2024-04-09 12:30:14 +0000

    add new cli functionality

--- mylib/logic.py
+++ mylib/logic.py
@@ -3,3 +3,7 @@ import wikipedia
 
 def wiki(name="War Goddess", length=1):
     return wikipedia.summary(name, length)
+
+
+def search_wiki(name):
+    return wikipedia.search(name)
--- test_logic.py
+++ test_logic.py
@@ -1,5 +1,9 @@
-from mylib.logic import wiki
+from mylib.logic import wiki, search_wiki
 
 
 def test_wiki():
     assert "god" in wiki()
+
+
+def test_wiki_search():
+    assert "National Basketball Association" in search_wiki("NBA")
